a68562cacb2f9308e97ada8d3a16141fdd857b9c
jkennyh <kenny.rabaye@gmail.com>
2017-02-08 21:54:14 +0100

    App created

---
+++ src/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author kenny
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
